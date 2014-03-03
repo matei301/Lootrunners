8a393170480c9b2556faa5e17f65934f318789b8
TumbaBit <mail@tumbabit.com>
2014-03-03 20:30:34 +0100

    Update EventListener.java

--- LootRunners/src/sunfury/plugin/lootrunners/EventListener.java
+++ LootRunners/src/sunfury/plugin/lootrunners/EventListener.java
@@ -1,7 +1,46 @@
 package sunfury.plugin.lootrunners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.List;
 
 public class EventListener implements Listener {
+    @EventHandler
+    public void onBlockPlace (BlockPlaceEvent event) {
+        if (Main.getInstance().getChestSetters().containsKey(event.getPlayer())) {
+            if (event.getBlock().getType() == Material.CHEST) {
+                List<String> list = Main.getInstance().getConfig().getStringList("LootRunners.Templates.Template" + Main.getInstance().getChestSetters().get(event.getPlayer()));
+                for (String string: list) {
+                    if (string.equals(event.getBlock().getX() + ", " + event.getBlock().getY() + ", " + event.getBlock().getZ())) {
+                        event.getPlayer().sendMessage(ChatColor.RED + "A chest has already been added at this location!");
+                    } else {
+                        list.add(string);
+                        Main.getInstance().getConfig().set("LootRunners.Templates.Template" + Main.getInstance().getChestSetters().get(event.getPlayer()), list);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    @EventHandler
+    public void onBlockBreak (BlockBreakEvent event) {
+        if (Main.getInstance().getChestSetters().containsKey(event.getPlayer())) {
+            if (event.getBlock().getType() == Material.CHEST) {
+                List<String> list = Main.getInstance().getConfig().getStringList("LootRunners.Templates.Template" + Main.getInstance().getChestSetters().get(event.getPlayer()));
+                for (String string: list) {
+                    if (string.equals(event.getBlock().getX() + ", " + event.getBlock().getY() + ", " + event.getBlock().getZ())) {
+                        list.remove(string);
+                        Main.getInstance().getConfig().set("LootRunners.Templates.Template" + Main.getInstance().getChestSetters().get(event.getPlayer()), list);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
